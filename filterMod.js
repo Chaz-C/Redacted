7510769df8f14af092a891e0e9882411b6624eb8
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2017-02-04 09:54:55 -1000

    connected module and message route handled

---
+++ filterMod.js
@@ -0,0 +1,5 @@
+function filter(req, res) {
+  return console.log(req.body);
+}
+
+module.exports = filter;
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const filter = require('./filterMod');
 
 app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use('/', (req,res, next) => {
+  filter(req, res);
+  next();
+});
+
+app.post('/message', (req,res) => {
+  console.log('in post');
+  res.end();
+});
 
 module.exports = app;
